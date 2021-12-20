1c559c609000ed7a777abf88ee6792122e54c8b5
mojtabaakbari221b <mojtaba.akbari.221b@gmail.com>
2021-12-20 16:50:02 +0330

    edit config for support drf

--- src/robotic/settings.py
+++ src/robotic/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'ckeditor',
     'blog',
 ]
@@ -147,3 +148,9 @@ CKEDITOR_UPLOAD_PATH = "uploads/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
