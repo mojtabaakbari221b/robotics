b4b7ec84e88fa2904f2a8eca111e08fe1f4efad7
mojtabaakbari221b <mojtaba.akbari.221b@gmail.com>
2021-12-22 23:06:37 +0330

    add filering package to drf

--- src/robotic/settings.py
+++ src/robotic/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_filters',
     'rest_framework',
     'ckeditor',
     'blog',
