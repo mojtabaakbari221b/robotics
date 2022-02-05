f88072240815bcb5df8806bf547b0db3f9546b4f
mojtabaakbari221b <mojtaba.akbari.221b@gmail.com>
2022-02-05 12:08:46 +0330

    edit view's ordering

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from url_filter.integrations.drf import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 from .models import (
     Group,
     Category,
@@ -32,72 +33,99 @@ class GroupViewSet(viewsets.ModelViewSet):
     serializer_class = GroupSerializer
     filter_backends = [
         DjangoFilterBackend,
+        OrderingFilter,
     ]
     filter_fields = '__all__'
+    ordering_fields = '__all__'
+    ordering = '?'
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all().select_related('group')
     serializer_class = CategorySerializer
     filter_backends = [
         DjangoFilterBackend,
+        OrderingFilter,
     ]
     filter_fields = '__all__'
+    ordering_fields = '__all__'
+    ordering = '?'
 
 class OrganViewSet(viewsets.ModelViewSet):
     queryset = Organ.objects.all().prefetch_related('gallery')
     serializer_class = OrganSerializer
     filter_backends = [
         DjangoFilterBackend,
+        OrderingFilter,
     ]
     filter_fields = '__all__'
+    ordering_fields = '__all__'
+    ordering = '?'
 
 class InfoViewSet(viewsets.ModelViewSet):
     queryset = Info.objects.all().select_related('organ')
     serializer_class = InfoSerializer
     filter_backends = [
         DjangoFilterBackend,
+        OrderingFilter,
     ]
     filter_fields = '__all__'
+    ordering_fields = '__all__'
+    ordering = '?'
 
 class ContactViewSet(viewsets.ModelViewSet):
     queryset = Contact.objects.all().select_related('organ')
     serializer_class = ContactSerializer
     filter_backends = [
         DjangoFilterBackend,
+        OrderingFilter,
     ]
     filter_fields = '__all__'
+    ordering_fields = '__all__'
+    ordering = '?'
 
 class StandardsViewSet(viewsets.ModelViewSet):
     queryset = Standards.objects.all().select_related('organ')
     serializer_class = StandardsSerializer
     filter_backends = [
         DjangoFilterBackend,
+        OrderingFilter,
     ]
     filter_fields = '__all__'
+    ordering_fields = '__all__'
+    ordering = '?'
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all().prefetch_related('category__group' , 'gallery').select_related('organ')
     serializer_class = ProductSerializer
     filter_backends = [
         DjangoFilterBackend,
+        OrderingFilter,
     ]
     filter_fields = '__all__'
+    ordering_fields = '__all__'
+    ordering = '?'
 
 class NewsViewSet(viewsets.ModelViewSet):
     queryset = News.objects.all()
     serializer_class = NewsSerializer
     filter_backends = [
         DjangoFilterBackend,
+        OrderingFilter,
     ]
     filter_fields = '__all__'
+    ordering_fields = '__all__'
+    ordering = '?'
 
 class RequirementsViewSet(viewsets.ModelViewSet):
     queryset = Requirements.objects.all()
     serializer_class = RequirementsSerializer
     filter_backends = [
         DjangoFilterBackend,
+        OrderingFilter,
     ]
     filter_fields = '__all__'
+    ordering_fields = '__all__'
+    ordering = '?'
 
 
 class SiteSupporterViewSet(viewsets.ModelViewSet):
@@ -105,13 +133,19 @@ class SiteSupporterViewSet(viewsets.ModelViewSet):
     serializer_class = SiteSupporterSerializer
     filter_backends = [
         DjangoFilterBackend,
+        OrderingFilter,
     ]
     filter_fields = '__all__'
+    ordering_fields = '__all__'
+    ordering = '?'
 
 class PageViewSet(viewsets.ModelViewSet):
     queryset = Page.objects.all()
     serializer_class = PageSerializer
     filter_backends = [
         DjangoFilterBackend,
+        OrderingFilter,
     ]
     filter_fields = '__all__'
+    ordering_fields = '__all__'
+    ordering = '?'
