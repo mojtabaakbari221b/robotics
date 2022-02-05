028f89b1d4ebc2aebb42c6355a88fc3431c36a16
mojtabaakbari221b <mojtaba.akbari.221b@gmail.com>
2022-02-05 12:09:03 +0330

    add api to prefix

--- src/robotic/urls.py
+++ src/robotic/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path , include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('api/admin/', admin.site.urls),
+    path('api/', include('blog.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
