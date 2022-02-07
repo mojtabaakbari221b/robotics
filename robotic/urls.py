8f8f7983e696d500ac987da9ddb1d57d1ce0b7e9
mojtabaakbari221b <mojtaba.akbari.221b@gmail.com>
2022-02-07 13:42:37 +0330

    edir urls

--- robotic/urls.py
+++ robotic/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path , include
 
 urlpatterns = [
-    path('api/admin/', admin.site.urls),
-    path('api/', include('blog.urls')),
+    path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
